f02900be0227e941f616533cc0f9fbe9627eea0a
Biswajit Jana <51967298+BlackBull-Rider@users.noreply.github.com>
2026-06-21 02:40:14 +0530

    Add load_symbol function to save indicators
    Implement load_symbol function to load and save indicators for a given symbol.

---
+++ data/load_indicators.py
@@ -0,0 +1,58 @@
+from core.db import get_connection
+from indicators.calculate import add_ema
+
+import pandas as pd
+
+
+def load_symbol(symbol):
+    conn = get_connection()
+
+    query = """
+    SELECT date, close
+    FROM historical_data
+    WHERE symbol = ?
+    ORDER BY date
+    """
+
+    df = pd.read_sql_query(
+        query,
+        conn,
+        params=(symbol,)
+    )
+
+    df.rename(
+        columns={
+            "close": "Close"
+        },
+        inplace=True
+    )
+
+    df = add_ema(df)
+
+    cur = conn.cursor()
+
+    for _, row in df.iterrows():
+
+        cur.execute(
+            """
+            INSERT OR REPLACE INTO indicators
+            (
+                symbol,
+                date,
+                ema20,
+                ema50
+            )
+            VALUES (?, ?, ?, ?)
+            """,
+            (
+                symbol,
+                row["date"],
+                float(row["EMA20"]),
+                float(row["EMA50"])
+            )
+        )
+
+    conn.commit()
+    conn.close()
+
+    print(symbol, "indicator saved")

26b0f177cdd9af922d27a09a5a6d10f24f02209d
Biswajit Jana <51967298+BlackBull-Rider@users.noreply.github.com>
2026-06-21 02:39:42 +0530

    Add function to calculate EMA for stock data

---
+++ indicators/calculate.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def add_ema(df):
+    df = df.copy()
+
+    df["EMA20"] = df["Close"].ewm(span=20, adjust=False).mean()
+    df["EMA50"] = df["Close"].ewm(span=50, adjust=False).mean()
+
+    return df

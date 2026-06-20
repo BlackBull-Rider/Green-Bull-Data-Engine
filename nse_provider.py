78b3a43e437a6353ae006bfbe587b64d91877375
Biswajit Jana <51967298+BlackBull-Rider@users.noreply.github.com>
2026-06-20 21:25:04 +0530

    Create NSEProvider class with placeholder methods

---
+++ nse_provider.py
@@ -0,0 +1,12 @@
+# providers/nse_provider.py
+
+from providers.base_provider import BaseProvider
+
+
+class NSEProvider(BaseProvider):
+
+    def get_daily_data(self, date):
+        pass
+
+    def get_history(self, symbol):
+        pass

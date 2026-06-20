00c77aaa81f5ce70e391d68e583040d3203c07b4
Biswajit Jana <51967298+BlackBull-Rider@users.noreply.github.com>
2026-06-21 02:26:14 +0530

    Update load_history.py

--- data/load_history.py
+++ data/load_history.py
@@ -1,71 +1,62 @@
-from providers.yahoo_provider import (
-    YahooProvider
-)
-
+from providers.yahoo_provider import YahooProvider
 from core.db import get_connection
 
-
-def save_history(
-    symbol,
-    df
-):
-
+def save_history(symbol, df):
     conn = get_connection()
-
     cur = conn.cursor()
 
     for date, row in df.iterrows():
-
-        cur.execute(
-            """
-            INSERT OR REPLACE INTO
-            historical_data
-            (
-                symbol,
-                date,
-                open,
-                high,
-                low,
-                close,
-                volume
-            )
-            VALUES
-            (?, ?, ?, ?, ?, ?, ?)
-            """,
-            (
-                symbol,
-                str(date.date()),
-                float(row["Open"]),
-                float(row["High"]),
-                float(row["Low"]),
-                float(row["Close"]),
-                float(row["Volume"])
-            )
+        cur.execute("""
+        INSERT OR REPLACE INTO historical_data
+        (
+            symbol,
+            date,
+            open,
+            high,
+            low,
+            close,
+            volume
         )
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+        """,
+        (
+            symbol,
+            str(date.date()),
+            float(row["Open"]),
+            float(row["High"]),
+            float(row["Low"]),
+            float(row["Close"]),
+            float(row["Volume"])
+        ))
 
     conn.commit()
     conn.close()
 
+def load_symbol(symbol):
+    provider = YahooProvider()
+
+    df = provider.get_history(symbol)
+
+    save_history(symbol, df)
+
+    print(f"{symbol} saved : {len(df)} rows")
 
 def main():
 
-    symbol = "RELIANCE"
+    symbols = [
+        "RELIANCE",
+        "TCS",
+        "INFY",
+        "HDFCBANK",
+        "ICICIBANK",
+        "SBIN"
+    ]
 
-    provider = YahooProvider()
-
-    df = provider.get_history(
-        symbol
-    )
-
-    save_history(
-        symbol,
-        df
-    )
-
-    print(
-        f"{symbol} saved : {len(df)} rows"
-    )
-
+    for symbol in symbols:
+        try:
+            load_symbol(symbol)
+        except Exception as e:
+            print(symbol, e)
 
 if __name__ == "__main__":
     main()
